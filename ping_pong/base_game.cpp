f4a7f0f91ebe1c69ca1c44e43adc7bd92d911352
baduliii <pramanishsharmabordoloi@gmail.com>
2025-08-10 20:09:15 +0530

    fully working base model :kekw:

--- ping_pong/base_game.cpp
+++ ping_pong/base_game.cpp
@@ -3,6 +3,41 @@
 
 using namespace std;
 
+//ball class to give ball some attibutes
+
+class Ball {
+public:
+	float x, y;
+	int radius;
+	int speed_x, speed_y;
+
+	void Draw() {
+		DrawCircle(x, y, radius, RAYWHITE);
+	}
+	
+	//update method to get the ball movin' dawg!
+
+	void Update() {
+		x += speed_x;
+		y += speed_y;
+
+		if (y + radius >= GetScreenHeight() || y - radius <= 0)
+		{
+			speed_y *= -1;
+		}
+
+		if (x + radius >= GetScreenWidth() || x - radius <= 0)
+		{
+			speed_x *= -1;
+		}
+	}
+};
+	
+
+
+//ball instance
+Ball ball;
+
 int main()
 
 {
@@ -13,7 +48,14 @@ int main()
 
 
 	SetTargetFPS(60);
+	//initialzing ball//
 
+	ball.radius = 20;
+	ball.x = screen_width / 2;
+	ball.y = screen_height / 2;
+	ball.speed_x = 7;
+	ball.speed_y = 7;
+	//end initialization
 
 	InitWindow(screen_width,screen_height, "pong game");
 
@@ -22,12 +64,18 @@ int main()
 	while (WindowShouldClose() == false)
 	{
 		BeginDrawing();
+
+		//upadate ball call
+
+		ball.Update();
+
+		DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
 		
-		DrawCircle(screen_width / 2, screen_height / 2, 20, RAYWHITE);
+		ball.Draw();
 
 		DrawRectangle(15, screen_height / 2 - 60, 25, 120, RAYWHITE);
 		DrawRectangle(screen_width-40, screen_height / 2 - 60, 25, 120, RAYWHITE);
-
+		ClearBackground(BLACK);
 
 
 		EndDrawing();
